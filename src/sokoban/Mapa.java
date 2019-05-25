0f6e4b9dc004830dd91e207a8a741754165496d8
EDDER <EDDER@DESKTOP-7SLF5TD>
2019-05-24 21:41:42 -0500

    mapa 2,3 y parte del 4 con función nodo meta con caja

--- src/sokoban/Mapa.java
+++ src/sokoban/Mapa.java
@@ -5,6 +5,9 @@
  */
 package sokoban;
 
+import sobras.Nodo;
+import sobras.Grafo;
+
 /**
  *
  * @author EDDER
@@ -31,63 +34,10 @@ public class Mapa {
         this.suelo = new Grafo(x, y);
         this.jugador = suelo.getInicio(); //Nodo inicial donde empieza el jugador
     }
-    
-    public Nodo hacerNodoConCaja(int x,int y){
-        Nodo nuevo;
-        nuevo = new Nodo(x, y);
-        nuevo.setTieneCaja(true);
-        for (int i = 0; i < this.cajas.length; i++) {
-            if (this.cajas[i]==null) {
-                this.cajas[i]=nuevo;
-                return nuevo;
-            }
-        }
-        return nuevo;
+        
+    public void cambiarPosCaja(){
     }
-    
-    public Nodo hacerNodoMeta(int x,int y){
-        Nodo nuevo;
-        nuevo = new Nodo(x,y);
-        nuevo.setEsMeta(true);
-        for (int i = 0; i < this.metas.length; i++) {
-            if (this.metas[i]==null) {
-                this.metas[i]=nuevo;
-                return nuevo;
-            }
-        }
-        return nuevo;
-    }
-    
-    public Nodo hacerNodoMetaConCaja(int x,int y){
-        Nodo nuevo;
-        nuevo = new Nodo(x,y);
-        nuevo.setEsMeta(true);
-        nuevo.setTieneCaja(true);
-       for (int i = 0; i < this.cajas.length; i++) {
-            if (this.cajas[i]==null) {
-                this.cajas[i]=nuevo;
-                break;
-            }
-       }
-        for (int i = 0; i < this.metas.length; i++) {
-            if (this.metas[i]==null) {
-                this.metas[i]=nuevo;
-                return nuevo;
-            }
-        }
-        return nuevo;
-    }
-    
-    public Nodo hacerNodo(int x,int y){
-        Nodo nuevo = new Nodo(x,y);
-        return nuevo;
-    }
-    public void cambiarPosCaja(int nodoActual,Nodo destino){
-        this.cajas[nodoActual].setTieneCaja(false);
-        destino.setTieneCaja(true);
-        this.cajas[nodoActual] = destino;   
-    }
-    public void cambiarPosJugador(Nodo destino){
-        this.jugador = destino;
+    public void cambiarPosJugador(){
+        
     }
 }
