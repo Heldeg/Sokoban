729e0bff7fa7cb15cb31a415b60e051e101ceedd
EDDER <EDDER@DESKTOP-7SLF5TD>
2019-05-25 15:26:29 -0500

    Lectura string

--- src/sokoban/Mapa.java
+++ src/sokoban/Mapa.java
@@ -5,6 +5,7 @@
  */
 package sokoban;
 
+import java.util.Scanner;
 import sobras.Nodo;
 import sobras.Grafo;
 
@@ -19,25 +20,65 @@ import sobras.Grafo;
 */
 public class Mapa {
     /*
-    *cajas   -> arreglo de nodos que tienen una caja
-    *jugador -> posicion del jugador
-    *metas   -> arreglo de nodos que son metas
-    *suelo   -> grafo que representa un nivel
+    *suelo   -> Matriz
+    ** -1 -> pared
+    **  0 -> suelo
+    **  1 -> Meta
     */
-    Nodo [] cajas;
-    Nodo jugador;
-    Nodo [] metas;
-    Grafo suelo;
-    public Mapa(int numCajas,int x,int y){
-        this.cajas = new Nodo[numCajas];
-        this.metas = new Nodo[numCajas];
-        this.suelo = new Grafo(x, y);
-        this.jugador = suelo.getInicio(); //Nodo inicial donde empieza el jugador
+    private short [][] suelo;
+    private final int n;
+    private final int m;
+    private Scanner scanner;
+    private final String estructura;
+    public Mapa(String estructura,int n,int m){
+        this.suelo = new short[n][m];
+        this.m = m;
+        this.n = n;
+        this.estructura=estructura;
+       
     }
+    private short obtenerNumero(int index){
+        this.scanner = new Scanner(this.estructura);
+        short valor=-2;
+        String aux;
+        char caracter = scanner.nextLine().charAt(index);
+        if(caracter=='-'){
+            valor=-1;
+        }else if(Character.isDigit(caracter)){
+            aux= Character.toString(caracter);
+            valor = Short.parseShort(aux);
+        }
+        return valor;
+    }
+    
+    public void insertarNumeros(){
+        int j=0,k=0;
+        short valor;
+        for (int i = 0; i < this.estructura.length(); i++) {  
+            valor = obtenerNumero(i);
+            if (valor!=-2) {
+                if(!(k<m)){
+                    k=0;
+                    j+=1;
+                }
+                this.suelo[j][k] = valor;
+                if(valor==-1){
+                    i+=1;
+                }
+                k+=1;
+            }
+        }
+    }
+    
+    
+    public void imprimeMatriz(){
+        for (int i = 0; i < this.n; i++) {
+            for (int j = 0; j < this.n; j++) {
+                System.out.print(this.suelo[i][j]+" ");
+            }
+            System.out.println("");
+        }
+    }
+    
         
-    public void cambiarPosCaja(){
-    }
-    public void cambiarPosJugador(){
-        
-    }
 }
